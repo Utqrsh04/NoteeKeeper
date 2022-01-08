323032ff0b477a7036950a80199bbd1232094d58
Utkarsh Vishwakarma <utkarshrkt2001@gmail.com>
2022-01-08 22:11:25 +0530

    Setup Tailwind CSS , Added Header

---
+++ frontend/src/components/Header/Header.js
@@ -0,0 +1,180 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <>
+      {/* <!-- component --> */}
+      <nav className="bg-white shadow">
+        <div className="container mx-auto px-6 py-3 md:flex md:justify-between md:items-center">
+          <div className="flex justify-between items-center">
+            <div>
+              <a
+                className="text-gray-800 text-xl font-bold md:text-2xl hover:text-gray-700"
+                href=" "
+              >
+                Brand
+              </a>
+            </div>
+
+            {/* Search input on desktop */}
+            <div className="hidden mx-10 md:block">
+              <div className="relative">
+                <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                  <svg
+                    className="w-5 h-5 text-gray-400"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                  >
+                    <path
+                      d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    ></path>
+                  </svg>
+                </span>
+
+                <input
+                  type="text"
+                  className="w-full py-2 pl-10 pr-4 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+                  placeholder="Search"
+                />
+              </div>
+            </div>
+            {/* <!-- Mobile menu button --> */}
+            <div className="flex md:hidden">
+              <button
+                type="button"
+                className="text-gray-500 hover:text-gray-600 focus:outline-none focus:text-gray-600"
+                aria-label="toggle menu"
+              >
+                <svg viewBox="0 0 24 24" className="h-6 w-6 fill-current">
+                  <path
+                    fill-rule="evenodd"
+                    d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
+                  ></path>
+                </svg>
+              </button>
+            </div>
+          </div>
+
+          {/* <!-- Mobile Menu open: "block", Menu closed: "hidden" --> */}
+          <div className="md:flex items-center">
+            <div className="flex flex-col md:flex-row md:mx-6">
+              <a
+                className="my-1 text-sm text-gray-700 font-medium hover:text-indigo-500 md:mx-4 md:my-0"
+                href=" "
+              >
+                My Notes
+              </a>
+            </div>
+
+            <div className="mt-3 md:hidden">
+              <div className="relative">
+                <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                  <svg
+                    className="w-5 h-5 text-gray-400"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                  >
+                    <path
+                      d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    ></path>
+                  </svg>
+                </span>
+
+                <input
+                  type="text"
+                  className="w-full py-2 pl-10 pr-4 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+                  placeholder="Search here"
+                />
+              </div>
+            </div>
+
+            {/* DropDown */}
+            <div className=" bg-white flex flex-col justify-center mt-2 md:mt-0 ">
+              <div className="flex items-center justify-center ">
+                <div className=" relative inline-block text-left dropdown">
+                  <span className="rounded-md shadow-sm">
+                    <button
+                      className="inline-flex justify-center w-full px-4 py-2 text-sm font-medium leading-5 text-gray-700 transition duration-150 ease-in-out bg-white border border-gray-300 rounded-md hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800"
+                      type="button"
+                      aria-haspopup="true"
+                      aria-expanded="true"
+                      aria-controls="headlessui-menu-items-117"
+                    >
+                      <span>User</span>
+                      <svg
+                        className="w-5 h-5 ml-2 -mr-1"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                          clip-rule="evenodd"
+                        ></path>
+                      </svg>
+                    </button>
+                  </span>
+
+                  <div className="opacity-0 invisible dropdown-menu transition-all duration-300 transform origin-top-right -translate-y-2 scale-95">
+                    <div
+                      className="absolute -right-3/4 md:right-0 w-56 mt-2 origin-top-right bg-white border border-gray-200 divide-y divide-gray-100 rounded-md shadow-lg outline-none"
+                      aria-labelledby="headlessui-menu-button-1"
+                      id="headlessui-menu-items-117"
+                      role="menu"
+                    >
+                      <div className="px-4 py-3">
+                        <p className="text-sm leading-5">Signed in as</p>
+                        <p className="text-sm font-medium leading-5 text-gray-900 truncate">
+                          tom@example.com
+                        </p>
+                      </div>
+                      <div className="py-1">
+                        <a
+                          href=" "
+                          tabindex="0"
+                          className="text-gray-700 flex justify-between w-full px-4 py-2 text-sm leading-5 text-left"
+                          role="menuitem"
+                        >
+                          Account settings
+                        </a>
+                        <a
+                          href=" "
+                          tabindex="1"
+                          className="text-gray-700 flex justify-between w-full px-4 py-2 text-sm leading-5 text-left"
+                          role="menuitem"
+                        >
+                          Support
+                        </a>
+                      </div>
+                      <div className="py-1">
+                        <a
+                          href=" "
+                          tabindex="3"
+                          className="text-gray-700 flex justify-between w-full px-4 py-2 text-sm leading-5 text-left"
+                          role="menuitem"
+                        >
+                          Sign out
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Header;
