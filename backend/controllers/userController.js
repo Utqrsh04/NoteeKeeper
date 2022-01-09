49b571de1dda20dd7effc70a9b0a7ca9a8cf0cb4
Utkarsh Vishwakarma <utkarshrkt2001@gmail.com>
2022-01-09 15:07:33 +0530

    Connected MongoDB database and Setup Backend User Authentication (routes , controller , middleware)

---
+++ backend/controllers/userController.js
@@ -0,0 +1,53 @@
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password, image } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User Already Exists");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+    image,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      image: user.image,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Error Ocuured!");
+  }
+});
+
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      image: user.image,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid Credentials!");
+  }
+});
+module.exports = { registerUser, authUser };
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const notes = require("./data/notes");
 const app = express();
+const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
 
 const dotenv = require("dotenv");
+const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
 dotenv.config();
+connectDB();
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Api working");
@@ -13,9 +18,10 @@ app.get("/api/notes", (req, res) => {
   res.json(notes);
 });
 
-app.get("/api/notes/:id", (req, res) => {
-  res.json("Notes by id");
-});
+app.use("/api/users", userRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
