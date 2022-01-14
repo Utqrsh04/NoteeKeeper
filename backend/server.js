dfd67c2734b770496945dbe133eebaf7abd4c5d0
Utkarsh Vishwakarma <utkarshrkt2001@gmail.com>
2022-01-14 15:43:15 +0530

    Implemented routes for Notes and CRUD operation for NOtes

--- backend/server.js
+++ backend/server.js
@@ -3,22 +3,24 @@ const notes = require("./data/notes");
 const app = express();
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
+const noteRoutes = require("./routes/noteRoutes");
+const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
 
 const dotenv = require("dotenv");
-const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
 dotenv.config();
 connectDB();
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Api working");
-});
+// app.get("/", (req, res) => {
+//   res.send("Api working");
+// });
 
-app.get("/api/notes", (req, res) => {
-  res.json(notes);
-});
+// app.get("/api/notes", (req, res) => {
+//   res.json(notes);
+// });
 
 app.use("/api/users", userRoutes);
+app.use("/api/notes", noteRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
